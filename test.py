afb559dd7e6b2bea2f025d927f745dea34504352
Dean Mills <deansmills@gmail.com>
2020-05-27 23:15:35 +0200

    found code to plot Rosenbrock function

--- test.py
+++ test.py
@@ -1,23 +1,53 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+# men_means = [20, 35, 30, 35, 27]
+# women_means = [25, 32, 34, 20, 25]
+# men_std = [2, 3, 4, 1, 2]
+# women_std = [3, 5, 2, 3, 3]
+# width = 0.35       # the width of the bars: can also be len(x) sequence
+
+# a= np.zeros(5)
+# print(a)
+# fig, ax = plt.subplots()
+
+# ax.bar(labels, men_means, width, yerr=men_std, label='Men')
+# ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
+#        label='Women')
+
+# ax.set_ylabel('Scores')
+# ax.set_title('Scores by group and gender')
+# ax.legend()
+
+# plt.show()
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plot
 import numpy as np
-import matplotlib.pyplot as plt
 
-labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-men_means = [20, 35, 30, 35, 27]
-women_means = [25, 32, 34, 20, 25]
-men_std = [2, 3, 4, 1, 2]
-women_std = [3, 5, 2, 3, 3]
-width = 0.35       # the width of the bars: can also be len(x) sequence
+fig = plot.figure()
+ax = fig.gca(projection='3d')
 
-a= np.zeros(5)
-print(a)
-fig, ax = plt.subplots()
+s = 0.05   # Try s=1, 0.25, 0.1, or 0.05
+X = np.arange(-2, 2.+s, s)   #Could use linspace instead if dividing
+Y = np.arange(-2, 3.+s, s)   #evenly instead of stepping...
+    
+#Create the mesh grid(s) for all X/Y combos.
+X, Y = np.meshgrid(X, Y)
 
-ax.bar(labels, men_means, width, yerr=men_std, label='Men')
-ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
-       label='Women')
+#Rosenbrock function w/ two parameters using numpy Arrays
+Z = (1.-X)**2 + 100.*(Y-X*X)**2
 
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.legend()
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+         linewidth=0, antialiased=False)  #Try coolwarm vs jet
 
-plt.show()
+ 
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+#Displays the figure, handles user interface, returns when user closes window
+plot.show()
